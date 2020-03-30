5527237c1625e64bb0e5e7c7f5d124be5de990c0
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-03-30 17:02:51 +0800

    Added controller.js

---
+++ controllers/controller.js
@@ -0,0 +1,23 @@
+
+// defines an object which contains functions executed as callback
+// when a client requests for `index` paths in the server
+const controller = {
+
+    // executed when the client sends an HTTP GET request `/favicon.ico`
+    // as defined in `../routes/routes.js`
+    getFavicon: function (req, res) {
+        res.status(204);
+    },
+
+    // executed when the client sends an HTTP GET request `/`
+    // as defined in `../routes/routes.js`
+    getIndex: function (req, res) {
+
+        // render `../views/index.hbs`
+        res.render('index');
+    }
+}
+
+// exports the object `controller` (defined above)
+// when another script exports from this file
+module.exports = controller;
