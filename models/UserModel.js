b284c85a4c4f4445dc7090c45a94b0fff24f2d25
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-03-30 17:11:55 +0800

    Added UserModel.js

---
+++ models/UserModel.js
@@ -0,0 +1,29 @@
+
+// import module `mongoose`
+var mongoose = require('mongoose');
+
+// defines the schema for collection `users`
+var UserSchema = new mongoose.Schema({
+    fName: {
+        type: String,
+        required: true
+    },
+    lName: {
+        type: String,
+        required: true
+    },
+    idNum: {
+        type: Number,
+        required: true
+    },
+    pw: {
+        type: String,
+        required: true
+    }
+});
+
+// exports a mongoose.model object based on `UserSchema` (defined above)
+// when another script exports from this file
+// This model executes CRUD operations
+// to collection `users` -> plural of the argument `User`
+module.exports = mongoose.model('User', UserSchema);
